707f779e775b2dcf5b2f5b82ecd39e100cfbb10f
Marquitus007 <2004marquitusrgmail.com>
2023-12-27 21:35:22 +0100

    commit 1 en marcos

---
+++ src/javamailej/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javamailej;
+
+/**
+ *
+ * @author 2004m
+ */
+public class NewClass {
+    
+}
